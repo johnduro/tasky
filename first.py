0e8e9398b8c83d0c6e5481f43305a9ca46989728
johnduro <martin.leroy6@gmail.com>
2015-05-12 18:30:22 +0200

    ca avance

--- first.py
+++ first.py
@@ -3,23 +3,71 @@
 
 import yaml
 import argparse
-# import subprocess
+import subprocess
 import sys
-# import psutil
+import psutil
+import datetime
+import time
 
 from subprocess import call
 
 class _TaskMaster:
     """class for program management"""
-    def __init__(self, conf):
+    def __init__( self, conf ):
+        # self.conf = {}
         self.conf = conf
 
-    def runTM(self):
+    def runTM( self ):
+        # print "HERE"
         self.initFirstLaunch()
+        # print "THERE"
+        self.makeLoop()
+        # print "HOHO"
+        # for (key, value) in self.conf['programs'].items():
+        #     for prog in value['proX']:
+        #         print prog[0]
+        #         print prog[1].pid
+        #         if prog[1].poll():
+        #             print "running"
 
-    def initFirstLaunch(self):
+    def makeLoop( self ):
+        while (42):
+            # print "in loop"
+            if not 'programs' in self.conf:
+                raise NameError("No 'programs' in configuration")
+            else:
+                self.managePrograms(self.conf['programs'])
+            # print "out loop"
+
+    def managePrograms( self, programs ):
+        # print "in manage"
+        for (key, value) in programs.items():
+            print key
+            for proX in value['proX']:
+                poll = proX[1].poll()
+                print "POLL"
+                print poll
+                if poll != None:
+                    if value['autoRestart'] == 'always':
+                        # print "ALWAYS"
+                        proX = (datetime, psutil.Popen(value['cmd'].split()))
+                    elif value['autoRestart'] == 'never':
+                        continue
+                    elif value['autoRestart'] == 'unexpected':
+                        # print "UNEXPECTED"
+                        if poll not in value['exitCodes']:
+                            proX = (datetime, psutil.Popen(value['cmd'].split()))
+
+
+    def initFirstLaunch( self ):
+        """lance tous les programs contenus dans self.conf['programs']"""
         if not 'programs' in self.conf:
             raise NameError("No 'programs' in configuration")
+        else:
+            for (key, value) in self.conf['programs'].items():
+                value['proX'] = []
+                value['proX'].append((datetime, psutil.Popen(value['cmd'].split())))
+                # value['proX']['pList'].append()
 
 def main():
     # programs = open("config.yaml", 'r')
@@ -34,7 +82,6 @@ def main():
             for _file in args.configuration_file:
                 openConf = yaml.load(open(_file, 'r'))
                 for (key, value) in openConf.items():
-                    # print key
                     if not key in conf:
                         conf[key] = value
                     else:
